b2f463fd69576a83b16a66e7def046b42c129ad0
MKTKING631 <maddineni.srnivas@gmail.com>
2026-04-11 16:42:13 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,36 @@
+from flask import Flask, render_template, request
+import yfinance as yf
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET", "POST"])
+def home():
+    result = ""
+    trend = ""
+    signal = ""
+
+    if request.method == "POST":
+        stock = request.form["stock"]
+
+        try:
+            data = yf.Ticker(stock)
+            hist = data.history(period="7d")
+
+            prices = list(hist["Close"])
+
+            if prices[-1] > prices[0]:
+                trend = "UPTREND 📈"
+                signal = "BUY 🟢"
+            else:
+                trend = "DOWNTREND 📉"
+                signal = "SELL 🔴"
+
+            result = f"{stock} price: {round(prices[-1],2)}"
+
+        except:
+            result = "Error ❌"
+
+    return render_template("index.html", result=result, trend=trend, signal=signal)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
